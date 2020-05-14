e9d6fbb2a42bf0a5c5e01310119a74b4a1ce6e3d
rithika <rithika@umich.edu>
2020-05-14 13:45:55 -0400

    script

---
+++ script.py
@@ -0,0 +1,5 @@
+import pyautogui
+script = "I dont want a valentine I just want Valentino I just want the neck I poker face like a casino Deep throat, know I get it in like a free throw Shawty want some more She just cant seem to keep her knees closed"
+for x in script.split():
+	pyautogui.write(x)
+	pyautogui.press("enter")
